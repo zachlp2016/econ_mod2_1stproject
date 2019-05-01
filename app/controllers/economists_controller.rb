75dd5896505657cf35926d20c79fb78ba97cb801
Zach Leach <zachleach@techoverco.com>
2019-05-01 16:52:46 -0600

    Completed user story 6.

--- app/controllers/economists_controller.rb
+++ app/controllers/economists_controller.rb
@@ -2,6 +2,24 @@ class EconomistsController < ApplicationController
 
   def index
     @economists = Economist.all
+
+  end
+
+  def new
+    @economist = Economist.new
+  end
+
+  def create
+    @new_economist = Economist.create(strong_params)
+    @new_economist.save
     
+    redirect_to economists_path
+  end
+
+
+  private
+
+  def strong_params
+    params.require(:economist).permit(:name, :age, :city, :image)
   end
 end
